0bff42943dea5803d8847469fabe971e13e997b4
Mahesh Samaradiwakara <hesh.here@gmail.com>
2019-11-25 11:44:24 +1100

    importing config params and creds from elliotCred.json. Including Redis, aws for LEX and Polly, FB appId

---
+++ app/config/index.js
@@ -0,0 +1,15 @@
+const config = require("../../elliotCred.json");
+
+module.exports = () => {
+  const env = process.env.NODE_ENV;
+  const redis = config.redis;
+
+  const awsCredentials = config.awsCredentials;
+
+  return {
+    appName: "ELLIOT",
+    redis: redis[env],
+    awsCredentials: awsCredentials[env],
+    facebook: config.facebook
+  }
+};
