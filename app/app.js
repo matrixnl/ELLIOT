de5cdf1b1d73fb5b64b7517250cbb18a235a9849
Mahesh Samaradiwakara <hesh.here@gmail.com>
2019-11-25 14:03:49 +1100

    error handler

--- app/app.js
+++ app/app.js
@@ -49,3 +49,19 @@ app.use((req, res, next) => {
 
 const routes = require('./routes/index');
 app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+// error handler
+// no stacktraces leaked to user unless in development environment
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.send({
+    message: err.message,
+    error: (app.get('env') === 'development') ? err : {}
+  });
+});
